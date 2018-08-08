5ab4879c0f6037162e3daede01b540779135d285
“Tyler <tylerpalef@gmail.com>
2018-08-08 16:28:35 -0400

    added dynamic route and controller for show.html.erb

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -2,4 +2,7 @@ class EntriesController < ApplicationController
   def index
   end
 
+  def show
+  end 
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   resources :entries
+  
+  get '/show/:id' => 'entries#show', as: 'show'
+
 end
